144306719f3cdb30e466e0ff2297aed92423d340
DevBin <yannick.zimmer@gmx.de>
2021-04-22 10:35:27 +0200

    Update main.py
    Improved readability
    Added sysargs for output and separation

--- main.py
+++ main.py
@@ -3,17 +3,32 @@ import sys
 
 j = int(sys.argv[2])
 
-file = open(r'H:\projects\py\tokensys\hashes.fm', sys.argv[3])
+if str(sys.argv[4]) == 'm':
+    sep = '-'
+elif str(sys.argv[4]) == 's':
+    sep = ' '
+elif str(sys.argv[4]) == 'p':
+    sep = '.'
+elif str(sys.argv[4]) == 'n':
+    sep = ''
+
+floc = str(sys.argv[5])
+
+file = open(r'%s' %floc, sys.argv[3])
+
 if str(sys.argv[3]) == 'a':
     file.write('\n')
+
 for i in range(0, j):
     hash = hex(random.getrandbits(int(sys.argv[1])))[2:]
     while (len(hash)/3).is_integer() is False:
        hash = hex(random.getrandbits(int(sys.argv[1])))[2:]
-    spaced = '-'.join([hash[i:i+3] for i in range(0, len(hash), 3)])
+    spaced = sep.join([hash[i:i+3] for i in range(0, len(hash), 3)])
+
     if j-i == 1:
         file.write(spaced)
     else:
         file.write(spaced + '\n')
     print(i+1)
+    
 file.close()
